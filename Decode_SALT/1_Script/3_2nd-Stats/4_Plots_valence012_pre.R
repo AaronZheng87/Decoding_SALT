9a2431f6b7648b3cba38398aa4478c63f6b71fa2
AaronZheng87 <aaronzheng87@gmail.com>
2023-04-25 11:47:10 +0800

    multiple preprocessing

--- Decode_SALT/1_Script/3_2nd-Stats/4_Plots_valence012_pre.R
+++ Decode_SALT/1_Script/3_2nd-Stats/4_Plots_valence012_pre.R
@@ -1,22 +1,39 @@
 library(tidyverse)
 library(papaja)
 source("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/1_Script/3_2nd-Stats/0_func.R")
-p <- "/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012/3_Plots"
+p <- "/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012_pre/3_Plots"
 ###################################################
 ############### Logistic Regression ##############
 ###################################################
 
 ####### Current trial
-ROC_within_c <- read_csv("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012/1_Logistic/1_Current/LR_withinscore_current.csv")
-ROC_cross_c <- read_csv(("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012/1_Logistic/1_Current/LR_crossscore_current.csv"))
+ROC_within_c <- read_csv("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012_pre/1_Logistic/1_Current/LR_withinscore_current.csv")
+ROC_cross_c <- read_csv(("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012_pre/1_Logistic/1_Current/LR_crossscore_current.csv"))
 ###### trials back
-ROC_within_tb <- read_csv("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012/1_Logistic/2_Trials-Back/LR_withinscore.csv")
-ROC_cross_tb <- read_csv("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012/1_Logistic/2_Trials-Back/LR_crossscore.csv")
+ROC_within_tb <- read_csv("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012_pre/1_Logistic/2_Trials-Back/LR_withinscore.csv")
+ROC_cross_tb <- read_csv("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012_pre/1_Logistic/2_Trials-Back/LR_crossscore.csv")
 roc_within_current_plot <- roc_violin_within(ROC_within_c)
 roc_within_tb_plot <- roc_violin_within(ROC_within_tb)
 
 roc_cross_current_plot <- roc_violin_cross(ROC_cross_c)
 roc_cross_tb_plot <- roc_violin_cross(ROC_cross_tb)
+
+ROC_within_c %>% 
+  group_by(source) %>% 
+  summarise(m = mean(score))
+
+ROC_cross_c %>% 
+  group_by(source, target) %>% 
+  summarise(m = mean(score))
+
+ROC_within_tb %>% 
+  group_by(source) %>% 
+  summarise(m = mean(score))
+
+ROC_cross_tb %>% 
+  group_by(source, target) %>% 
+  summarise(m = mean(score))
+
 
 ggsave("roc_within_current.png", plot = roc_within_current_plot, path = p, width = 10, height = 7)
 ggsave("roc_cross_current.png", plot = roc_cross_current_plot, path = p, width = 10, height = 7)
@@ -25,11 +42,11 @@ ggsave("roc_cross_tb.png", plot = roc_cross_tb_plot, path = p, width = 10, heigh
 
 ################## feature importance####################
 ### current trial
-within_im_current <- read_csv("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Log_prepro/1_Logistic/1_Current/importance_within_current.csv")
-cross_im_current <- read_csv("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012/1_Logistic/1_Current/importance_cross_current.csv")
+within_im_current <- read_csv("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012_pre/1_Logistic/1_Current/importance_within_current.csv")
+cross_im_current <- read_csv("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012_pre/1_Logistic/1_Current/importance_cross_current.csv")
 ### Trials back
-within_im_tb <- read_csv("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012/1_Logistic/2_Trials-Back/importance_within.csv")
-cross_im_tb <- read_csv("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012/1_Logistic/2_Trials-Back/importance_cross.csv")
+within_im_tb <- read_csv("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012_pre/1_Logistic/2_Trials-Back/importance_within.csv")
+cross_im_tb <- read_csv("/Users/zhengyuanrui/Decoding_SALT/Decode_SALT/3_Result/Valence012_pre/1_Logistic/2_Trials-Back/importance_cross.csv")
 
 
 im_within_current_plot <- fi_within_current(within_im_current)
